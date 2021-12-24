a776274bdee2a609ce3a3611981e69267d993dcb
Deepak5597 <32607176+Deepak5597@users.noreply.github.com>
2021-12-24 12:59:17 +0530

    Adding Footer File

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+include('./_partials/head.php');
+?>
+<footer class="container-fluid bg-dark text-center p-2">
+          <small class="text-white">Copyright &copy; 2018 || Designed By ELearning ||
+            <a href="./adminlogin.php">Admin Login</a>
+          </small>
