2acf1614c6bde46b35cb883926ed0e5a2ad0e9db
Divyabisht0505 <bisht.divya761@gmail.com>
2021-12-24 13:12:25 +0530

    Changes footer yesr from 2018 to 2019

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 include('./_partials/head.php');
 ?>
 <footer class="container-fluid bg-dark text-center p-2">
-          <small class="text-white">Copyright &copy; 2018 || Designed By ELearning ||
+          <small class="text-white">Copyright &copy; 2019 || Designed By ELearning ||
             <a href="./adminlogin.php">Admin Login</a>
           </small>
